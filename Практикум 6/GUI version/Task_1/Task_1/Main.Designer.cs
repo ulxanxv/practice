605d3f65d624183a2d75bbe1e5ff34162b965525
Артём <@DESKTOP-SL90R76>
2020-05-11 15:48:26 +0300

    Добавлена вторая часть 1 задания

--- Практикум 6/GUI version/Task_1/Task_1/Main.Designer.cs
+++ Практикум 6/GUI version/Task_1/Task_1/Main.Designer.cs
@@ -29,9 +29,8 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.groupLabel = new System.Windows.Forms.ToolStripLabel();
             this.label1 = new System.Windows.Forms.Label();
-            this.inputArray = new System.Windows.Forms.RichTextBox();
-            this.outputArray = new System.Windows.Forms.RichTextBox();
-            this.resultButton = new System.Windows.Forms.Button();
+            this.input1DArray = new System.Windows.Forms.RichTextBox();
+            this.resultButton1D = new System.Windows.Forms.Button();
             this.outputResult = new System.Windows.Forms.TextBox();
             this.labelOutputResult = new System.Windows.Forms.Label();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
@@ -39,6 +38,11 @@
             this.inputB = new System.Windows.Forms.TextBox();
             this.labelA = new System.Windows.Forms.Label();
             this.labelB = new System.Windows.Forms.Label();
+            this.resultButton2D = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input2DArray = new System.Windows.Forms.RichTextBox();
+            this.outputArray = new System.Windows.Forms.TextBox();
+            this.outputArrayLabel = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +57,7 @@
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(558, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(764, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "infoPanel";
             // 
@@ -79,52 +83,42 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 19);
+            this.label1.Size = new System.Drawing.Size(252, 19);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Введите массив —";
+            this.label1.Text = "Введите массив (одномерный)";
             // 
-            // inputArray
+            // input1DArray
             // 
-            this.inputArray.Location = new System.Drawing.Point(13, 57);
-            this.inputArray.Name = "inputArray";
-            this.inputArray.Size = new System.Drawing.Size(208, 292);
-            this.inputArray.TabIndex = 2;
-            this.inputArray.Text = "";
+            this.input1DArray.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input1DArray.Location = new System.Drawing.Point(12, 60);
+            this.input1DArray.Name = "input1DArray";
+            this.input1DArray.Size = new System.Drawing.Size(252, 166);
+            this.input1DArray.TabIndex = 2;
+            this.input1DArray.Text = "";
             // 
-            // outputArray
+            // resultButton1D
             // 
-            this.outputArray.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.outputArray.Location = new System.Drawing.Point(227, 130);
-            this.outputArray.Name = "outputArray";
-            this.outputArray.ReadOnly = true;
-            this.outputArray.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.outputArray.Size = new System.Drawing.Size(318, 96);
-            this.outputArray.TabIndex = 3;
-            this.outputArray.Text = "";
-            // 
-            // resultButton
-            // 
-            this.resultButton.Location = new System.Drawing.Point(227, 232);
-            this.resultButton.Name = "resultButton";
-            this.resultButton.Size = new System.Drawing.Size(318, 30);
-            this.resultButton.TabIndex = 4;
-            this.resultButton.Text = "Подсчитать!";
-            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton1D.Location = new System.Drawing.Point(12, 232);
+            this.resultButton1D.Name = "resultButton1D";
+            this.resultButton1D.Size = new System.Drawing.Size(593, 30);
+            this.resultButton1D.TabIndex = 4;
+            this.resultButton1D.Text = "Подсчитать (в одномерном)!";
+            this.resultButton1D.UseVisualStyleBackColor = true;
             // 
             // outputResult
             // 
             this.outputResult.Enabled = false;
-            this.outputResult.Location = new System.Drawing.Point(227, 323);
+            this.outputResult.Location = new System.Drawing.Point(12, 323);
             this.outputResult.Name = "outputResult";
             this.outputResult.ReadOnly = true;
-            this.outputResult.Size = new System.Drawing.Size(318, 26);
+            this.outputResult.Size = new System.Drawing.Size(681, 26);
             this.outputResult.TabIndex = 5;
             this.outputResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // labelOutputResult
             // 
             this.labelOutputResult.AutoSize = true;
-            this.labelOutputResult.Location = new System.Drawing.Point(223, 301);
+            this.labelOutputResult.Location = new System.Drawing.Point(699, 326);
             this.labelOutputResult.Name = "labelOutputResult";
             this.labelOutputResult.Size = new System.Drawing.Size(54, 19);
             this.labelOutputResult.TabIndex = 6;
@@ -132,14 +126,14 @@
             // 
             // inputA
             // 
-            this.inputA.Location = new System.Drawing.Point(228, 57);
+            this.inputA.Location = new System.Drawing.Point(611, 235);
             this.inputA.Name = "inputA";
             this.inputA.Size = new System.Drawing.Size(100, 26);
             this.inputA.TabIndex = 7;
             // 
             // inputB
             // 
-            this.inputB.Location = new System.Drawing.Point(445, 57);
+            this.inputB.Location = new System.Drawing.Point(611, 271);
             this.inputB.Name = "inputB";
             this.inputB.Size = new System.Drawing.Size(100, 26);
             this.inputB.TabIndex = 8;
@@ -147,7 +141,7 @@
             // labelA
             // 
             this.labelA.AutoSize = true;
-            this.labelA.Location = new System.Drawing.Point(334, 60);
+            this.labelA.Location = new System.Drawing.Point(717, 238);
             this.labelA.Name = "labelA";
             this.labelA.Size = new System.Drawing.Size(36, 19);
             this.labelA.TabIndex = 9;
@@ -156,34 +150,86 @@
             // labelB
             // 
             this.labelB.AutoSize = true;
-            this.labelB.Location = new System.Drawing.Point(403, 60);
+            this.labelB.Location = new System.Drawing.Point(717, 278);
             this.labelB.Name = "labelB";
             this.labelB.Size = new System.Drawing.Size(36, 19);
             this.labelB.TabIndex = 10;
-            this.labelB.Text = "B —";
+            this.labelB.Text = "— B";
+            // 
+            // resultButton2D
+            // 
+            this.resultButton2D.Location = new System.Drawing.Point(12, 268);
+            this.resultButton2D.Name = "resultButton2D";
+            this.resultButton2D.Size = new System.Drawing.Size(593, 30);
+            this.resultButton2D.TabIndex = 4;
+            this.resultButton2D.Text = "Подсчитать (в двумерном)!";
+            this.resultButton2D.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите массив (двумерный)";
+            // 
+            // input2DArray
+            // 
+            this.input2DArray.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input2DArray.Location = new System.Drawing.Point(270, 60);
+            this.input2DArray.Name = "input2DArray";
+            this.input2DArray.Size = new System.Drawing.Size(252, 166);
+            this.input2DArray.TabIndex = 2;
+            this.input2DArray.Text = "";
+            // 
+            // outputArray
+            // 
+            this.outputArray.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputArray.Location = new System.Drawing.Point(528, 60);
+            this.outputArray.Multiline = true;
+            this.outputArray.Name = "outputArray";
+            this.outputArray.ReadOnly = true;
+            this.outputArray.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputArray.Size = new System.Drawing.Size(225, 166);
+            this.outputArray.TabIndex = 11;
+            this.outputArray.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputArrayLabel
+            // 
+            this.outputArrayLabel.AutoSize = true;
+            this.outputArrayLabel.Location = new System.Drawing.Point(627, 34);
+            this.outputArrayLabel.Name = "outputArrayLabel";
+            this.outputArrayLabel.Size = new System.Drawing.Size(126, 19);
+            this.outputArrayLabel.TabIndex = 12;
+            this.outputArrayLabel.Text = "Вывод массива";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(558, 361);
+            this.ClientSize = new System.Drawing.Size(764, 361);
+            this.Controls.Add(this.outputArrayLabel);
+            this.Controls.Add(this.outputArray);
             this.Controls.Add(this.labelB);
             this.Controls.Add(this.labelA);
             this.Controls.Add(this.inputB);
             this.Controls.Add(this.inputA);
             this.Controls.Add(this.labelOutputResult);
             this.Controls.Add(this.outputResult);
-            this.Controls.Add(this.resultButton);
-            this.Controls.Add(this.outputArray);
-            this.Controls.Add(this.inputArray);
+            this.Controls.Add(this.resultButton2D);
+            this.Controls.Add(this.resultButton1D);
+            this.Controls.Add(this.input2DArray);
+            this.Controls.Add(this.input1DArray);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStrip1);
             this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(574, 400);
-            this.MinimumSize = new System.Drawing.Size(574, 400);
+            this.MaximumSize = new System.Drawing.Size(780, 400);
+            this.MinimumSize = new System.Drawing.Size(780, 400);
             this.Name = "Main";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -202,9 +248,8 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripLabel groupLabel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox inputArray;
-        private System.Windows.Forms.RichTextBox outputArray;
-        private System.Windows.Forms.Button resultButton;
+        private System.Windows.Forms.RichTextBox input1DArray;
+        private System.Windows.Forms.Button resultButton1D;
         private System.Windows.Forms.TextBox outputResult;
         private System.Windows.Forms.Label labelOutputResult;
         private System.Windows.Forms.ToolTip toolTip1;
@@ -212,6 +257,11 @@
         private System.Windows.Forms.TextBox inputB;
         private System.Windows.Forms.Label labelA;
         private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Button resultButton2D;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox input2DArray;
+        private System.Windows.Forms.TextBox outputArray;
+        private System.Windows.Forms.Label outputArrayLabel;
     }
 }
 
--- Практикум 6/GUI version/Task_1/Task_1/Main.cs
+++ Практикум 6/GUI version/Task_1/Task_1/Main.cs
@@ -7,17 +7,23 @@ namespace Task_1 {
             InitializeComponent();
 
             // Обработчик на нажатие по кнопке
-            resultButton.Click += OutButton_Click;
+            resultButton1D.Click += ResultButton1D_Click;
+            resultButton2D.Click += ResultButton2D_Click;
 
             // Всплывающая подсказка о том, как вводить массив
-            toolTip1.SetToolTip(this.inputArray, "Пример ввода массива — 1 4 2 5 6 3 2");
+            toolTip1.SetToolTip(this.input1DArray, "Пример ввода массива:\n1 4 2 5 6 3 2");
+            toolTip1.SetToolTip(this.input2DArray, "Пример ввода массива:\n1 4 2\n5 6 3\n2 5 1");
         }
 
-        private void OutButton_Click(object sender, EventArgs e) {
-            result();
+        private void ResultButton2D_Click(object sender, EventArgs e) {
+            result2D();
         }
 
-        private void result() {
+        private void ResultButton1D_Click(object sender, EventArgs e) {
+            result1D();
+        }
+
+        private void result1D() {
             int[] array;
 
             array = getArray();
@@ -30,6 +36,26 @@ namespace Task_1 {
                     print(array);
 
                     outputResult.Text = "Чисел — " + countNumbers(array, a , b);
+                } catch (Exception) {
+                    outputResult.Text = "Неверный диапазон значений!";
+                    return;
+                }
+            }
+        }
+
+        private void result2D() {
+            int[,] array;
+
+            getArray(out array);
+
+            if (array != null) {
+                try {
+                    int a = int.Parse(inputA.Text),
+                        b = int.Parse(inputB.Text);
+
+                    print(array);
+
+                    outputResult.Text = "Чисел — " + countNumbers(array, a, b);
                 } catch (Exception) {
                     outputResult.Text = "Неверный диапазон значений!";
                     return;
@@ -50,15 +76,14 @@ namespace Task_1 {
         }
 
         private void print(int[] array) {
-            outputArray.Text = "Ваш массив :\n";
             for (int z = 0; z < array.Length; ++z) {
                 outputArray.Text += array[z] + " ";
             }
         }
 
         private int[] getArray() {
-            string[] infoArray  = inputArray.Text.Trim().Split(' ');
-            int[] array         = new int[inputArray.Text.Trim().Split(' ').Length];
+            string[] infoArray  = input1DArray.Text.Trim().Split(' ');
+            int[] array         = new int[input1DArray.Text.Trim().Split(' ').Length];
 
             try {
                 for (int z = 0; z < array.Length; ++z) {
@@ -70,6 +95,57 @@ namespace Task_1 {
             }
 
             return array;
+        }
+
+        private int countNumbers(int[,] array, int a, int b) {
+            int count = 0;
+
+            for (int z = 0; z < array.GetLength(0); ++z) {
+                for (int y = 0; y < array.GetLength(1); ++y) {
+                    if (a <= array[z, y] && array[z, y] <= b) {
+                        ++count;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void print(int[,] array) {
+            outputArray.Text = "";
+
+            for (int z = 0; z < array.GetLength(0); ++z) {
+                for (int y = 0; y < array.GetLength(1); ++y) {
+                    outputArray.Text += array[z, y] + " ";
+                }
+                outputArray.Text += "\r\n";
+            }
+        }
+
+        private bool getArray(out int[,] array) {
+            string[] infoArray  = input2DArray.Text.Trim().Split('\n');
+
+            int rows            = input2DArray.Text.Trim().Split('\n').Length;
+            int columns         = input2DArray.Text.Trim().Split('\n')[0].Split(' ').Length;
+
+            array               = new int[rows, columns];
+
+            string[] row;
+            try {
+                for (int z = 0; z < array.GetLength(0); ++z) {
+                    row = infoArray[z].Split(' ');
+                    for (int y = 0; y < array.GetLength(1); ++y) {
+                        array[z, y] = int.Parse(row[y]);
+                    }
+                }
+            } catch (Exception) {
+                outputArray.Text = "Неверные данные!";
+
+                array = null;
+                return false;
+            }
+
+            return true;
         }
     }
 }

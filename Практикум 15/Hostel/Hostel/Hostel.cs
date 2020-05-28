fe2b284a4ef630efbd83d5280dc791f4d0983c78
Артём <@DESKTOP-SL90R76>
2020-05-28 16:41:14 +0300

    Добавлено выполненное задание 15-го практикума

---
+++ Практикум 15/Hostel/Hostel/Hostel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hostel {
+    public partial class Hostel : Form {
+        public Hostel() {
+            InitializeComponent();
+
+            exitButton.Click += ExitButton_Click;
+
+            studentsButton.Click += StudentsButton_Click;
+            roomsButton.Click += RoomsButton_Click;
+            privilegesButton.Click += PrivilegesButton_Click;
+            stratificationButton.Click += StratificationButton_Click;
+        }
+
+        private void StratificationButton_Click(object sender, EventArgs e) {
+            new Stratification().ShowDialog();
+        }
+
+        private void PrivilegesButton_Click(object sender, EventArgs e) {
+            new Privileges().ShowDialog();
+        }
+
+        private void RoomsButton_Click(object sender, EventArgs e) {
+            new Rooms().ShowDialog();
+        }
+
+        private void StudentsButton_Click(object sender, EventArgs e) {
+            new Students().ShowDialog();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
---
+++ Практикум 15/Hostel/Hostel/Stratification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hostel {
+    public partial class Stratification : Form {
+        public Stratification() {
+            InitializeComponent();
+
+            exitButton.Click += ExitButton_Click;
+            confirmChanges.Click += ConfirmChanges_Click;
+        }
+
+        private void ConfirmChanges_Click(object sender, EventArgs e) {
+            roomsBindingSource.EndEdit();
+            roomsTableAdapter.Update(hostelDataSet);
+            studentsTableAdapter.Update(hostelDataSet);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void roomsBindingNavigatorSaveItem_Click(object sender, EventArgs e) {
+            this.Validate();
+            this.roomsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.hostelDataSet);
+
+        }
+
+        private void Stratification_Load(object sender, EventArgs e) {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "hostelDataSet.Students". При необходимости она может быть перемещена или удалена.
+            this.studentsTableAdapter.Fill(this.hostelDataSet.Students);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "hostelDataSet.Rooms". При необходимости она может быть перемещена или удалена.
+            this.roomsTableAdapter.Fill(this.hostelDataSet.Rooms);
+
+        }
+    }
+}
